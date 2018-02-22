58b398312100b455381b3fd8b2c94343d70fdaaf
BlueStone1995 <Pierre.Mbape@etu.univ-paris1.fr>
2018-02-22 13:06:16 +0100

    Version non fonctionnel du FOSUserBundle avant changement depuis le départ

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,8 +8,44 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContext;
 
-class UserController
+
+class UserController extends Controller
 {
+
+    /**
+     * @Route("/", name="FOSUserBundle_Security_login")
+     * @Method({"GET", "POST"})
+     */
+    public function loginAction(Request $request)
+    {
+
+        if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            // retourne au menu
+            return $this->redirectToRoute('menu');
+        } else {
+            // retourne à l'accueil
+            return $this->render('FOSUserBundle/Security/login.html.twig', [
+                'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+            ]);
+        }
+
+    }
+
+    /**
+     * @Route("/menu", name="menu")
+     *
+     */
+    public function menuAction(Request $request)
+    {
+        return $this->render('menu.html.twig');
+    }
 
 }
