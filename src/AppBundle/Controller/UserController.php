46c94dc1c36cc10a5ec8180f685b808fd017f9b9
didicluber <dylanouanounou@yahoo.fr>
2018-02-26 21:53:19 +0100

    fix problem of FosUserBundle (don't forget to update schema of db & to composer install - update)

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,44 +8,38 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\SecurityContext;
 
 
 class UserController extends Controller
 {
-
-    /**
-     * @Route("/", name="FOSUserBundle_Security_login")
-     * @Method({"GET", "POST"})
-     */
-    public function loginAction(Request $request)
-    {
-
-        if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            // retourne au menu
-            return $this->redirectToRoute('menu');
-        } else {
-            // retourne à l'accueil
-            return $this->render('FOSUserBundle/Security/login.html.twig', [
-                'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
-            ]);
-        }
-
-    }
-
-    /**
-     * @Route("/menu", name="menu")
-     *
-     */
-    public function menuAction(Request $request)
-    {
-        return $this->render('menu.html.twig');
-    }
+//
+//    /**
+//     * @Route("/", name="FOSUserBundle_Security_login")
+//     * @Method({"GET", "POST"})
+//     */
+//    public function loginAction(Request $request)
+//    {
+//
+//        if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+//            // retourne au menu
+//            return $this->redirectToRoute('menu');
+//        } else {
+//            // retourne à l'accueil
+//            return $this->render('FOSUserBundle/Security/login.html.twig', [
+//                'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+//            ]);
+//        }
+//
+//    }
+//
+//    /**
+//     * @Route("/menu", name="menu")
+//     *
+//     */
+//    public function menuAction(Request $request)
+//    {
+//        return $this->render('menu.html.twig');
+//    }
 
 }
