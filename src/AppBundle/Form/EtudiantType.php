e87c96d257e8ce841bc71f00809084a4cdcfc500
Yue HONG <hongyue192447@gmail.com>
2018-03-05 22:55:17 +0100

    Ajouté une entité Etudiant edit et show : pour les users new : à faire auto-générer juste après l'inscription index : faut les cacher, disponibles uniquement pour l'administration

---
+++ src/AppBundle/Form/EtudiantType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EtudiantType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('id')->add('iNE')->add('mobile')->add('mailaltern')->add('diplomeEnCours')->add('departement')->add('adresse');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Etudiant'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_etudiant';
+    }
+
+
+}
