586092e0d8f89283e52dec9981c33ff0fc6bb736
BlueStone1995 <Pierre.Mbape@etu.univ-paris1.fr>
2018-03-07 20:52:27 +0100

    Ajout fonctionnalité Annuaire V1.2.0

--- src/AppBundle/Controller/AnnuaireController.php
+++ src/AppBundle/Controller/AnnuaireController.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -28,15 +29,31 @@ class AnnuaireController extends Controller
     }
 
     /**
-     * @Route("/annuaire/{id}", name="annuaire_fiche")
-     * @Method({"GET", "POST"})
+     * @Route("/annuaire/fiche", name="annuaire_fiche")
+     * @Method({"GET"})
      */
-    public function ficheAction(User $user)
+    public function ficheAction(Request $request)
     {
-        // retourne page user de l'annuaire
-        return $this->render('annuaire/fiche.html.twig', array(
-            'user' => $user
-        ));
+        // Récupère paramètre de mon url
+        $nom = $request->query->get('nom');
 
+        $em = $this->getDoctrine()->getManager();
+
+        // Cherche utilisateur grâce au nom
+        $user = $em->getRepository('AppBundle:User')->findOneBy(array('nom' => $nom)); // Renvoie un objet
+        $userId = $user->getId();
+
+        // Cherche etudiant grace à l'id user
+        $etudiant = $em->getRepository('AppBundle:Etudiant')->find($userId);
+
+        if ($user != null && $etudiant != null) {
+            // retourne page fiche de l'annuaire
+            return $this->render('annuaire/fiche.html.twig', array(
+                'user' => $user,
+                'etudiant' => $etudiant,
+            ));
+        } else {
+            return $this->render('annuaire/recherche.html.twig');
+        }
     }
 }
