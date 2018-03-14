bb90b26e62ff123327a41fff1be55fdde862e897
BlueStone1995 <Pierre.Mbape@etu.univ-paris1.fr>
2018-03-14 16:11:28 +0100

    Modification BDD pour User date de naissance en type datetime Modification lié à datetime V1.2.3

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
@@ -15,27 +16,37 @@ class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nom', CollectionType::class, array(
-            'entry_type' => TextType::class,
-            'entry_options' => array(
-                'attr' => array(
-                    'class' => 'email-box',
-                    'label' => 'Nom',
-                )
-            )));
+        $builder->add('nom', TextType::class, array(
+            'label' => 'Nom',
+        ));
         $builder->add('prenom', TextType::class, array(
             'label' => 'Prénom',
         ));
         $builder->add('dnaiss', BirthdayType::class, array(
-            'label' => 'Date de naissance',
+            'label' => 'Date de naissance (jj/mm/aaaa)',
             'placeholder' => 'Sélectionner une valeur',
             'widget' => 'single_text',
             'format' => 'dd-MM-yyyy',
         ));
-        $builder->add('genre', RadioType::class, array(
-            'label' => 'Genre',
-        ));
-        $builder->remove('username');
+
+        $builder->add('genre');
+
+        /**
+         * $builder->add('genre', CollectionType::class, array(
+         * 'entry_type' => ChoiceType::class,
+         * 'entry_options' => array(
+         * 'label' => 'Genre (Homme/Femme)',
+         * 'choices' => array(
+         * 'Maybe' => null,
+         * 'Yes' => true,
+         * 'No' => false,
+         * ),
+         * ),
+         * ));
+         */
+
+        // Demande pas user
+        //$builder->remove('username');
     }
 
     public function getParent()
