00a8a38ea3b109db896b7e0ca11b4a47c7e8250d
BlueStone1995 <Pierre.Mbape@etu.univ-paris1.fr>
2018-03-17 15:20:02 +0100

    Modification vue réservation cours/ V1.3.3

--- src/AppBundle/Form/CoursType.php
+++ src/AppBundle/Form/CoursType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CoursType extends AbstractType
 {
@@ -13,16 +14,36 @@ class CoursType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('nomCours')
-            ->add('nomEns')
-            ->add('idEns')
-            ->add('nombreChx')
-            ->add('nombreTotal')
-            ->add('heure')
-            ->add('jour')
-            ->add('lieu')
-            ->add('credit');
-    }/**
+        $builder->add('nomCours', TextType::class, array(
+            'label' => 'Nom Cours',
+        ))
+            ->add('nomEns', TextType::class, array(
+                'label' => 'Nom Enseignant',
+            ))
+            ->add('idEns', TextType::class, array(
+                'label' => 'Id Enseignant',
+            ))
+            ->add('nombreChx', TextType::class, array(
+                'label' => 'Nombre place réservé',
+            ))
+            ->add('nombreTotal', TextType::class, array(
+                'label' => 'Nombre place disponible',
+            ))
+            ->add('heure', TextType::class, array(
+                'label' => 'Heure',
+            ))
+            ->add('jour', TextType::class, array(
+                'label' => 'Jour',
+            ))
+            ->add('lieu', TextType::class, array(
+                'label' => 'Lieu',
+            ))
+            ->add('credit', TextType::class, array(
+                'label' => 'Crédit ECTS',
+            ));
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
